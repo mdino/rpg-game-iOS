d0ad9645ed0a52154c68cc6ec31cd8498512e3d2
Dino <dino.musliu@outlook.com>
2016-07-11 19:11:12 +0200

    Initial Commit

---
+++ rpgoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpgoop
+//
+//  Created by Dino Musliu on 11/07/2016.
+//  Copyright © 2016 Dino Musliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
